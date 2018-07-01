7321d93f51d7079cecda7851e3e838209317ce0c
Maykon Anschau Oliveira <maykonoliveira@edu.univali.br>
2018-07-01 19:03:57 -0300

    Inicial

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# resource-sharing
-Trabalho M3 - SO
+# Informações do Projeto
+
+# Ambiente de Desenvolvimento
+
+## Requisitos
+
+- docker
+- docker-compose
+
+# Testes
+Efetuar o download do projeto:
+```
+git clone https://github.com/MaykonOliveira/resource-file-sharing.git
+```
+
+
+### Forma de execução do docker-compose app_no_lock
+```
+cd app_no_lock/
+docker-compose up
+```
+
+
+### Forma de execução do docker-compose app_lock
+```
+cd app_lock/
+docker-compose up
+```
+
---
+++ app_lock/app/app.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/file.h>
+
+#include <time.h>
+#include <sys/time.h>
+
+#define N 5
+
+int main()
+{
+    char hostname[1024];
+    gethostname(hostname, 1024);
+    int n = N;
+
+    FILE *arq;
+    
+    while(n--){
+        struct timeval uSec;
+        time_t current_time = time(0);
+        gettimeofday(&uSec, NULL);
+        struct tm *current = localtime(&current_time);
+        arq = fopen("/shared_file/file.txt", "a+");
+        flock(fileno(arq), LOCK_EX);
+        if (!arq){
+            perror("Arquivo inexistente");
+            exit(0);
+        }
+        fprintf(arq, "ID da maquina: %s - Hora: %d:%d:%d\n", hostname, current->tm_hour, current->tm_min, current->tm_sec);
+        sleep(3);
+        flock(fileno(arq), LOCK_UN);
+        fclose(arq);
+    }   
+    return 0;
+}
